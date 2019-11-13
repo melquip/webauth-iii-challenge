11cfea1cd1486abab86421a8072fc54190fa402f
Melquisedeque Pereira <melqui1@live.com.pt>
2019-11-13 12:46:41 +0000

    Added users table seed

---
+++ data/seeds/01-add-users.js
@@ -0,0 +1,16 @@
+const bcrypt = require('bcryptjs');
+exports.seed = function (knex) {
+  return knex('user').truncate()
+    .then(function () {
+      return knex('user').insert([
+        {
+          "username": "admin",
+          "password": bcrypt.hashSync('admin', 11),
+        },
+        {
+          "username": "melqui",
+          "password": bcrypt.hashSync('melqui', 11),
+        },
+      ]);
+    });
+};
